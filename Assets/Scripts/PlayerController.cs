eb0b9e379ac0ad13cfe23ac282d9c6e4804625dc
Alice201322 <evdokimova.alice@gmail.com>
2024-10-06 17:05:45 +0400

    Done

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private float speed = 5f;
+    public GameManager gameManager;
+    public GameObject gameOver;
 
     // Start is called before the first frame update
     void Start()
@@ -25,6 +27,36 @@ public class PlayerController : MonoBehaviour
         if (Input.GetKey(KeyCode.A))
         {
             transform.Translate(Vector3.left * speed * Time.deltaTime);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Coin"))
+        {
+            Debug.Log("Coin");
+            gameManager.coins++;
+            gameManager.coinText.text = "Coins: " + gameManager.coins;
+            Destroy(other.gameObject);
+        }
+
+        if (other.CompareTag("Up"))
+        {
+            transform.localScale += new Vector3(0.2f, 0.2f, 0.2f);
+        }
+
+        if (other.CompareTag("Down"))
+        {
+            transform.localScale -= new Vector3(0.1f, 0.1f, 0.1f);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.CompareTag("Finish"))
+        {
+            gameOver.SetActive(true);
+            speed = 0;
         }
     }
 }
